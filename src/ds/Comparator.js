536b3c892dd536c85eddf9071e8c52d7fb62056f
yerokha <aitkazy.smartex@gmail.com>
2019-11-15 15:19:51 +0600

    initial

---
+++ src/ds/Comparator.js
@@ -0,0 +1,39 @@
+class Comparator {
+	constructor(compareFunction) {
+		this.compare = compareFunction || Comparator.defaultComparatorFunction;
+	}
+
+	static defaultComparatorFunction(a, b) {
+		if (a === b) {
+			return 0;
+		}
+		return a < b ? -1 : 1;
+	}
+
+	equal(a, b) {
+		return this.compare(a, b) === 0;
+	}
+
+	lessThan(a, b) {
+		return this.compare(a, b) < 0;
+	}
+
+	greaterThan(a, b) {
+		return this.compare(a, b) > 0;
+	}
+
+	lessThanOrEqual(a, b) {
+		return this.lessThan(a, b) || this.equal(a, b);
+	}
+
+	greaterThanOrEqual(a, b) {
+		return this.greaterThan(a, b) || this.equal(a, b);
+	}
+
+	reverse() {
+		const compareOrigin = this.compare;
+		this.compare = (a, b) => compareOrigin(b, a);
+	}
+}
+
+export default Comparator;
